fe31075250616397a9b1bc6b86eed2e540632596
mhxx307 <minhquan.lavo@gmail.com>
2022-12-23 12:39:02 +0700

    animation and check correct word

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -11,14 +11,35 @@ interface SquareProps {
 const Square: React.FC<SquareProps> = ({ boardValue, position }) => {
     // redux state
     const positionRedux = useSelector((state: rootState) => state.board.position);
-    const rowRedux = useSelector((state: rootState) => state.board.row);
+    // const rowRedux = useSelector((state: rootState) => state.board.row);
     const correctWord = useSelector((state: rootState) => state.board.correctWord);
     // local state
     const [isCorrect, setIsCorrect] = useState<boolean>(false);
     const [isWrong, setIsWrong] = useState<boolean>(false);
     const [isAlmostCorrect, setIsAlmostCorrect] = useState<boolean>(false);
 
-    useEffect(() => {}, [boardValue]);
+    // reset position index to 0 when row change
+    const currentPosition = (positionRedux - 1) % 5;
+
+    useEffect(() => {
+        if (correctWord[currentPosition] === boardValue) setIsCorrect(true);
+        else if (!isCorrect && boardValue !== "" && correctWord.includes(boardValue))
+            setIsAlmostCorrect(true);
+        else if (
+            !isCorrect &&
+            !isAlmostCorrect &&
+            boardValue !== "" &&
+            !correctWord.includes(boardValue)
+        )
+            setIsWrong(true);
+
+        return () => {
+            setIsCorrect(false);
+            setIsWrong(false);
+            setIsAlmostCorrect(false);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [boardValue]);
 
     const variants = {
         filled: () => ({
@@ -34,12 +55,25 @@ const Square: React.FC<SquareProps> = ({ boardValue, position }) => {
             },
         }),
     };
+
+    const status: string = isCorrect
+        ? "correct"
+        : isAlmostCorrect
+        ? "almost"
+        : isWrong
+        ? "wrong"
+        : "";
+
     return (
         <motion.div
             animate={boardValue ? "filled" : "unfilled"}
             variants={variants}
-            className="h-[60px] flex justify-center items-center
-        rounded-sm border-[1px] border-[cyan] cursor-pointer"
+            className={`h-[60px] flex justify-center items-center
+        rounded-sm border-[1px] border-[cyan] cursor-pointer ${
+            (status === "correct" && "bg-[#10A19D]") ||
+            (status === "wrong" && "bg-[red]") ||
+            (status === "almost" && "bg-[yellow]")
+        }`}
         >
             {boardValue}
         </motion.div>
